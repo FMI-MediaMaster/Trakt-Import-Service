5b25a0c36fd8ab6e1f95c1231e7f4070e16fc8dd
mariaabarbarasa <maria.barbarasa@icloud.com>
2025-12-30 09:33:16 +0200

    [TEST] Add test for the import endpoints

---
+++ tests/controller.test.ts
@@ -0,0 +1,51 @@
+import runImportTests, { Fields } from '@media-master/import-service-tests';
+import { Express } from 'express';
+import { describe, it, expect } from 'vitest';
+import app from '../src/app';
+
+const server = app as Express;
+
+describe('Controller', () => {
+    describe('Endpoint /movies', () => {
+        const endpoint: string = '/movies';
+        const validIds: string[] = [
+            'blackspell',
+            'Duckyz95',
+            'Karmalakas',
+        ];
+        const invalidIds: string[] = [
+            '-1',
+            'Not an id',
+            'nonExistentId',
+        ];
+        const fields: Fields = {
+            name: { type: 'string' },
+        };
+        runImportTests(
+            server,
+            endpoint,
+            { validIds, invalidIds, fields }
+        );
+    });
+    describe('Endpoint /series', () => {
+        const endpoint: string = '/series';
+        const validIds: string[] = [
+            'blackspell',
+            'Duckyz95',
+            'Karmalakas',
+        ];
+        const invalidIds: string[] = [
+            '-1',
+            'Not an id',
+            'nonExistentId',
+        ];
+        const fields: Fields = {
+            name: { type: 'string' },
+        };
+        runImportTests(
+            server,
+            endpoint,
+            { validIds, invalidIds, fields }
+        );
+    });
+});
